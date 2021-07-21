623f7184ab48ddb1d6f3d18fcd3f67aa18ced099
Jacek Grzybowski <totospl99@gmail.com>
2021-07-22 00:06:20 +0200

    print devices example

---
+++ print_devices/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <CL/cl.hpp>
+#include <vector>
+
+int main()
+{
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    if(platforms.size() == 0)
+    {
+        std::cout << "No platforms found! Exiting..." << std::endl;
+        return 0;
+    }
+
+    std::cout << "Found platforms. Listing...\n";
+    for (auto platform : platforms)
+    {
+        std::cout << "platform:\t" << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
+        std::vector<cl::Device> devices;
+        platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+        if(devices.size() == 0)
+        {
+            std::cout << "\tNo devices for this platform." << std::endl;
+        }
+        else
+        {
+            std::cout << "\tDevices:" << std::endl;
+            for (auto device : devices)
+            {
+                std::cout << "\t" << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+            }
+        }
+    }
+}
